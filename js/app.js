cf0ecd197654a6e0d193b90b9cdc7589a97a880b
119kanika <kanika@users.noreply.github.com>
2021-04-07 14:25:32 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// navbar-js-start
+    //   $(document).ready(function(){
+    //     $('.btn').click(function(){
+    //       $('.items').toggleClass("show");
+    //       $('ul li').toggleClass("hide");
+    //     });
+    //   });
+// navbar-js-finis
