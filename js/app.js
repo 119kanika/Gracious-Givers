9bfc218343ccb0d616c497c0ae8e759a2ed1bc71
119kanika <kanika@users.noreply.github.com>
2021-04-09 14:03:33 +0530

    donate

--- js/app.js
+++
@@ -1,8 +0,0 @@
-// navbar-js-start
-    //   $(document).ready(function(){
-    //     $('.btn').click(function(){
-    //       $('.items').toggleClass("show");
-    //       $('ul li').toggleClass("hide");
-    //     });
-    //   });
-// navbar-js-finis
